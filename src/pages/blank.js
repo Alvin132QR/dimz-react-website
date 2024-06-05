28f5e7944b35cb37dc9ea1139dcbe93ac62e15f1
Alvin132QR <oa8929843@gmail.com>
2024-06-05 12:12:37 +0700

    Create blank.js

---
+++ src/pages/blank.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default NoPage;
+function NoPage() {
+  return (
+    <div id="eror" class="container mt-5">
+    <div class="card p-5 shdw mx-auto divk">
+    <div>
+      <h1>404</h1>
+      <p>Unknown Page</p>
+      <a href="/">Back</a>
+    </div>
+      </div>
+      </div>
+  );
+}
