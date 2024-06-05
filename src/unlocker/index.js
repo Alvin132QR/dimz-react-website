69d23a7bd6e26f61b6066836779f4da989e0e6a2
Alvin132QR <oa8929843@gmail.com>
2024-06-05 12:19:36 +0700

    Update index.js

--- src/unlocker/index.js
+++ src/unlocker/index.js
@@ -7,9 +7,19 @@ function UnlockPage() {
         <div class="blur-background" id="blurBackground"></div>
         
         <div class="download-panel" id="downloadPanel">
-            <button id="unlockerBtn" onclick={Ads}>Unlock Download</button>
+            <button id="unlockerBtn" onclick={Ads}>Unlock</button>
             <button id="downloadBtn" disabled onclick={Goto}>Dowload</button>
         </div>
    </div>
     )
+}
+
+function Ads() {
+   alert("Test")
+   document.getElementById("downloadBtn").disabled = false;
+}
+
+function Goto() {
+   alert("Dimz & Alvqid")
+   document.location = "dimzproject.my.id"
 }
