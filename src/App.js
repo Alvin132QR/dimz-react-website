1233acc930815101e984623322bd878db9de5dce
Alvin132QR <oa8929843@gmail.com>
2024-06-05 12:15:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home.js';
-import UnlockPage from './unclocker/index';
+import UnlockPage from './unlocker/index';
 import NoPage from './pages/blank.js';
 import './App.css';
 
